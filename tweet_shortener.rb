925894a0f45e5dd245da8ca3d8693e3adc0beba3
Aaron B <abriscoe2008@gmail.com>
2019-06-08 06:42:00 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,9 @@
 def dictionary 
   {
     "hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"
-  }
+    }
 end
+
+def word_substituter(tweet)
+  tweet.to_a.each do 
+

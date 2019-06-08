90d41521eebed878661e17cc28b6c67f95828cbd
Aaron B <abriscoe2008@gmail.com>
2019-06-08 23:25:40 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,21 @@
 def dictionary 
   {
     "hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"
-    }
+  }
 end
 
 def word_substituter(tweet)
-  tweet.to_a.each do 
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
 
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
